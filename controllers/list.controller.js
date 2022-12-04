39f2ca4e9a931efcb4eae37bf5781812a651d384
Divyansh Gandhi <dg1199@gmail.com>
2022-12-04 18:41:59 +0530

    Adding list routes and controllers

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -4,11 +4,42 @@ const Op = db.Sequelize.Op;
 
 // Retrieve all Lists from the DB for current user using a user ID
 exports.findAll = (req, res) => {
+  const user_id = req.query.user_id;
+  var condition = user_id ? { user_id: { [Op.eq]: `${user_id}` } } : null;
 
+  //Using Serialize to findAll Todos for a specific user
+  Todo.findAll({ where: condition })
+    .then(data => {
+      res.send(data);
+    })
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while retrieving tutorials."
+      });
+    });
 };
 
 // Find a single List of Todos using a List ID
 exports.findOne = (req, res) => {
+  const id = req.params.id;
+
+  // Find a Todo with an ID
+  Todo.findByPk(id)
+    .then(data => {
+      if (data) {
+        res.send(data);
+      } else {
+        res.status(404).send({
+          message: `Cannot find Todo with id=${id}.`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error retrieving Todo with id=" + id
+      });
+    });
 
 };
 
@@ -46,22 +77,89 @@ exports.create = (req, res) => {
       });
     }
     );
-
-
 };
 
 
 // Update a List using a List ID from request and the updated List item
 exports.update = (req, res) => {
+  const id = req.params.id;
+
+  // Update todo item with given ID
+  List.update(req.body, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "List was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update List with id=${id}. Maybe List was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating List with id=" + id
+      });
+    }
+    );
 
 };
 
 //Empty a List of Todos using a List ID from request
 exports.empty = (req, res) => {
+  const id = req.params.id;
+
+  // Update todo item with given ID
+  List.update({
+    todos: []
+  }, {
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "List was updated successfully."
+        });
+      } else {
+        res.send({
+          message: `Cannot update List with id=${id}. Maybe List was not found or req.body is empty!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Error updating List with id=" + id
+      });
+    }
+    );
 
 };
 
 // Delete an entire List using a List ID from request
 exports.delete = (req, res) => {
+  const id = req.params.id;
 
+  List.destroy({
+    where: { id: id }
+  })
+    .then(num => {
+      if (num == 1) {
+        res.send({
+          message: "List was deleted successfully!"
+        });
+      } else {
+        res.send({
+          message: `Cannot delete List with id=${id}. Maybe List was not found!`
+        });
+      }
+    })
+    .catch(err => {
+      res.status(500).send({
+        message: "Could not delete List with id=" + id
+      });
+    }
+    );
 };
