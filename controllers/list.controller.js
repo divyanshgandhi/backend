12d96f639cd483e76423aacaa5d7e4e745d89cba
Divyansh Gandhi <dg1199@gmail.com>
2022-12-04 16:25:51 +0530

    Updating routes

--- controllers/list.controller.js
+++ controllers/list.controller.js
@@ -4,12 +4,12 @@ const Op = db.Sequelize.Op;
 
 // Retrieve all Lists from the DB for current user using a user ID
 exports.findAll = (req, res) => {
-  
+
 };
 
 // Find a single List of Todos using a List ID
 exports.findOne = (req, res) => {
-  
+
 };
 
 // Create and save a new List item to the DB
@@ -23,16 +23,45 @@ exports.create = (req, res) => {
     return;
   }
 
-  
+  // Create a List
+  const list = {
+    title: req.body.title,
+    description: req.body.description,
+    todos: req.body.todos,
+    user_id: req.body.user_id,
+    created_at: req.body.created_at,
+    updated_at: req.body.updated_at
+  };
+
+  // Save List in the database
+  List.create(list)
+    .then(data => {
+      res.send(data);
+    }
+    )
+    .catch(err => {
+      res.status(500).send({
+        message:
+          err.message || "Some error occurred while creating the List."
+      });
+    }
+    );
+
+
 };
 
 
 // Update a List using a List ID from request and the updated List item
 exports.update = (req, res) => {
-  
+
+};
+
+//Empty a List of Todos using a List ID from request
+exports.empty = (req, res) => {
+
 };
 
 // Delete an entire List using a List ID from request
 exports.delete = (req, res) => {
-  
+
 };
--- controllers/todo.controller.js
+++ controllers/todo.controller.js
@@ -4,12 +4,38 @@ const Op = db.Sequelize.Op;
 
 // Retrieve all Todos from the DB for current user using a user ID
 exports.findAll = (req, res) => {
-  
+    const user_id = req.query.user_id;
+    var condition = user_id ? { user_id: { [Op.eq]: `${user_id}` } } : null;
+
+    //Using Serialize to findAll Todos for a specific user
+    Todo.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 };
 
 //Retrieve all Todos from a specific list using a list ID
 exports.findInList = (req, res) => {
+    const list_id = req.query.list_id;
+    var condition = list_id ? { lsit_id: { [Op.eq]: `${list_id}` } } : null;
 
+    //Using Serialize to findAll Todos for a specific list
+    Todo.findAll({ where: condition })
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message:
+                    err.message || "Some error occurred while retrieving tutorials."
+            });
+        });
 };
 
 // Find a single Todo using an ID
@@ -19,20 +45,20 @@ exports.findOne = (req, res) => {
     // Find a Todo with an ID
     Todo.findByPk(id)
         .then(data => {
-        if (data) {
-            res.send(data);
-        } else {
-            res.status(404).send({
-            message: `Cannot find Todo with id=${id}.`
-            });
-        }
+            if (data) {
+                res.send(data);
+            } else {
+                res.status(404).send({
+                    message: `Cannot find Todo with id=${id}.`
+                });
+            }
         })
         .catch(err => {
-        res.status(500).send({
-            message: "Error retrieving Todo with id=" + id
+            res.status(500).send({
+                message: "Error retrieving Todo with id=" + id
+            });
         });
-        });
-  
+
 };
 
 // Create and save a new Todo item to the DB
@@ -40,7 +66,7 @@ exports.create = (req, res) => {
     // Validate request
     if (!req.body.title) {
         res.status(400).send({
-        message: "Content can not be empty!"
+            message: "Content can not be empty!"
         });
         return;
     }
@@ -64,7 +90,7 @@ exports.create = (req, res) => {
         .catch(err => {
             res.status(500).send({
                 message:
-                err.message || "Some error occurred while creating the Todo."
+                    err.message || "Some error occurred while creating the Todo."
             });
         });
 };
@@ -72,10 +98,54 @@ exports.create = (req, res) => {
 
 // Update a Todo using an ID from request and the updated Todo item
 exports.update = (req, res) => {
-  
+    const id = req.params.id;
+
+    // Update todo item with given ID
+    Todo.update(req.body, {
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Todo was updated successfully."
+                });
+            } else {
+                res.send({
+                    message: `Cannot update Todo with id=${id}. Maybe Todo was not found or req.body is empty!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error updating Todo with id=" + id
+            });
+        }
+        );
+
 };
 
 // Delete a Todo using an ID from request
 exports.delete = (req, res) => {
-  
+    const id = req.params.id;
+
+    // Delete a Todo with the specified ID
+    Todo.destroy({
+        where: { id: id }
+    })
+        .then(num => {
+            if (num == 1) {
+                res.send({
+                    message: "Todo was deleted successfully!"
+                });
+            } else {
+                res.send({
+                    message: `Cannot delete Todo with id=${id}. Maybe Todo was not found!`
+                });
+            }
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Could not delete Todo with id=" + id
+            });
+        });
 };
